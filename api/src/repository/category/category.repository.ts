4e89fa987a437b10d2cbc8dab4bf550aac1d95ab
williamazevedo <william@vipcommerce.com.br>
2022-01-21 20:48:50 -0300

    improvements on json import

--- api/src/repository/category/category.repository.ts
+++ api/src/repository/category/category.repository.ts
@@ -1,7 +1,20 @@
 import { ICategoryRepository } from '..';
+import { MongoClient } from 'mongodb';
+import { Category } from '../../model';
 
-export class CategoryRepository implements ICategoryRepository{
-  async listCategories(){
-    return [];
+export class CategoryRepository implements ICategoryRepository {
+  async listCategories() {
+    try{
+      const connection = await MongoClient.connect(`mongodb://api:mashgin-checkout-api-123@${process.env.DB_HOST}:${process.env.DB_PORT}`);
+      const database = await connection.db('checkout-db');
+      const categories = await database.collection('Categories').find().toArray() as any;
+      return categories.map((category:{[key:string]:string|number|boolean})=>{
+        //const model:Category = {...category, id:category._id};
+       // delete (<unknown>model)._id;
+        return category;
+      })
+    }catch(e){
+      console.log(e);
+    }
   }
 }
--- database/mongo-init.js
+++ database/mongo-init.js
@@ -3,11 +3,23 @@ db = db.getSiblingDB('checkout-db');
 db.createCollection('Products');
 db.createCollection('Categories');
 
+function normalize(it){
+  it._id = it.id;
+  delete it.id;
+  return it;
+}
 
-const menu = JSON.parse(cat('/docker-entrypoint-initdb.d/sample-menu.json',true));
 
-print('----------------ITEMS:')
-print(menu)
+print('\n\n\n======= Initiating database with provided JSON =======\n\n');
+let menu = cat('/docker-entrypoint-initdb.d/sample-menu.json',true);
+print('JSON imported successfully!')
+print(menu);
 
-db.Products.insertMany(menu.items);
-db.Categories.insertMany(menu.categories);
+menu = JSON.parse(menu);
+
+db.Categories.insertMany(menu.categories.map(normalize));
+print('\n* Categories registered successfully!\n');
+db.Products.insertMany(menu.items.map(normalize));
+print('\n* Products registered successfully!\n');
+
+print('\n\n\n=======================================================\n\n');
