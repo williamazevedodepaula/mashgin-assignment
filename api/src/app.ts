ae819f883d43b120e7e02ed8fbac9351aad50709
williamazevedo <william@vipcommerce.com.br>
2022-01-23 14:35:34 -0300

    created homepage

--- api/src/app.ts
+++ api/src/app.ts
@@ -5,15 +5,21 @@ const app = express()
 const port = 3000
 
 
-app.get('/', (req:Request, res:Response) => {
-  res.send('Checkout Menu API!')
-})
-
 app.use(express.json())
 app.use('/menu',require('./router/menu.router'))
 app.use('/orders',require('./router/order.router'))
 app.use(express.static('/files'));
 
+app.get('/', (req:Request, res:Response) => {
+  res.send(`
+    <h1>Checkout Menu API!</h1>
+    <ul>
+      <li> Fetch menu: GET <a href="/menu">/menu</a></li>
+      <li> Put order: POST /orders
+      <li> Fetch images: /images/:imageId
+    </ul>
+  `)
+})
 
 app.listen(port, () => {
   console.log(`Application listening on port ${port}`)
