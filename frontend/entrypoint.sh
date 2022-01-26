a34c7adadc9db0bb418d41ab33a7b21eec8b55e5
williamazevedo <william@vipcommerce.com.br>
2022-01-25 21:22:42 -0300

    deploying frontend app and storybook in prod mode

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-npm install
 
 echo "Running entrypoint..."
 
-echo "1) Building react applcation"
-npm run build
-
-#if [ ! -d "/files/images/" ]; then
-#  cp /docker-entrypoint-initfiles.d/sample/sample-menu.json .storybook/resources/sample-menu.json
-#fi
 
 #Run container start command
 exec  "$@"
