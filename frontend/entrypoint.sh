c2c4479278d278df92ddce165d5ea18d4db9e4ea
williamazevedo <william@vipcommerce.com.br>
2022-01-26 21:06:37 -0300

    improvements in environment control

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -2,11 +2,14 @@
 
 echo "Running entrypoint..."
 
-rm ./static -Rf
-npm run build
-npm run build-storybook
-mv ./build/ ./static
-mv ./storybook-static ./static/storybook
+if [ "$ENVIRONMENT" != "dev" ]; then
+  echo "Not in development mode! Building the static files..."
+  rm ./static -Rf
+  npm run build
+  npm run build-storybook
+  mv ./build/ ./static
+  mv ./storybook-static ./static/storybook
+fi;
 
 #Run container start command
 exec  "$@"
